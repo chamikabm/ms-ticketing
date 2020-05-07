5db937a705e85f7a937865aa1b8650f5b7090f65
chamikabm <chamika@bitsmedia.com>
2020-05-07 15:37:26 +0530

    Added skaffold changes.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,6 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-
-
 app.listen(3000, () => {
-    console.log('Listen on port 3000');
+    console.log('Listening on port 3000');
 });
