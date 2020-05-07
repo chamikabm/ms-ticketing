a2dd3ac759acbdc4004cc9d01bb91757221ae12a
chamikabm <chamika@bitsmedia.com>
2020-05-07 22:36:20 +0530

    Added changes for sending custom error response and handling async errors.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import {signinRouter} from './routes/signin';
@@ -6,6 +7,7 @@ import {signupRouter} from './routes/signup';
 import {signoutRouter} from './routes/signout';
 
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -13,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+
+// Send 404 for all not found routes, 'all' for all type of HTTP methods, GET, POST, DELETE etc.
+app.all('*', async () => {
+    throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
