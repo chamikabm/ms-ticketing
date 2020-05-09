b3ca2b0240d23aa47b37b2a522bf28ca1f93ff4c
chamikabm <chamika@bitsmedia.com>
2020-05-09 16:41:53 +0530

    Moved request validation logic to a middleware.

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin',  (req, res) => {
+router.post('/api/users/signin', [
+        body('email')
+            .isEmail()
+            .withMessage('Email Must be Valid!'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must provide a password.')
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
     res.send('Hi there!')
 });
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,53 +1,50 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email Must be Valid!'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters.')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+        body('email')
+            .isEmail()
+            .withMessage('Email Must be Valid!'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters.')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
 
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array())
-    }
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            console.log('Email in use');
+            throw new BadRequestError('Email in user');
+        }
 
-    if (existingUser) {
-        console.log('Email in use');
-        throw new BadRequestError('Email in user');
-    }
+        const user = User.build({ email, password });
+        await user.save();
 
-    const user = User.build({ email, password });
-    await user.save();
+        // Generate JWT
+        const userJwt = jwt.sign({
+                id: user.id,
+                email: user.email,
+            }, process.env.JWT_KEY! // Here ! tells to typescript compiler not to worry about this value being null or undefined.
+        );
 
-    // Generate JWT
-    const userJwt = jwt.sign({
-            id: user.id,
-            email: user.email,
-        }, process.env.JWT_KEY! // Here ! tells to typescript compiler not to worry about this value being null or undefined.
-    );
+        // Store it on session object
+        // req.session.jwt = userJwt;
+        // We can't do live above as Typescript definition file for cookie-session
+        // does not have information
+        req.session = {
+            jwt: userJwt
+        };
 
-    // Store it on session object
-    // req.session.jwt = userJwt;
-    // We can't do live above as Typescript definition file for cookie-session
-    // does not have information
-    req.session = {
-        jwt: userJwt
-    };
-
-    res.status(201).send(user);
+        res.status(201).send(user);
 });
 
 export { router as signupRouter };
