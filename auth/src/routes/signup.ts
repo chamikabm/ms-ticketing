63ea56abcab0144e03f2e4c96ed28009d898ce53
chamikabm <chamika@bitsmedia.com>
2020-05-09 13:39:47 +0530

    Added changes for storing jwt in a cookie.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -30,6 +31,20 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email,
+    }, 'chamika');
+
+    // Store it on session object
+    // req.session.jwt = userJwt;
+    // We can't do live above as Typescript definition file for cookie-session
+    // does not have information
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
 });
