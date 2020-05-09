67f497964f629708176c95ff73682eadb6813d8f
chamikabm <chamika@bitsmedia.com>
2020-05-09 17:08:21 +0530

    Added changes for current user route.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser',  (req, res) => {
-    res.send('Hi there!')
+    // if(!req.session || !req.session.jwt) {  Same as below, ? typescript symbol to check internal property is available or not.
+    if(!req.session?.jwt) {
+        console.log('Seesion not found')
+        return res.send({ currentUser : null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        return res.send({ currentUser : payload });
+    } catch (err) {
+        console.log('Error payload')
+        return res.send({ currentUser : null });
+    }
 });
 
 export { router as currentUserRouter };
