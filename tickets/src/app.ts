081fe4c5a327fee86a0bc52e9649ad13040f0e1f
chamikabm <chamika@bitsmedia.com>
2020-05-11 20:50:54 +0530

    Ticketing serivce initial files added.

---
+++ tickets/src/app.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import 'express-async-errors'; // Use to handle async error
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, errorHandler } from '@ms-ticketing/common';
+
+const app = express();
+app.use(json({}));
+
+// Indicates the app is behind a front-facing proxy, and to use
+// the X-Forwarded-* headers to determine the connection and the
+// IP address of the client. NOTE: X-Forwarded-* headers are easily
+// spoofed and the detected IP addresses are unreliable.
+// This is a express setting: https://expressjs.com/en/api.html --> Application Settings
+
+// The X-Forwarded-For (XFF) header is a de-facto standard header
+// for identifying the originating IP address of a client connecting
+// to a web server through an HTTP proxy or a load balancer.
+// When traffic is intercepted between clients and servers,
+// server access logs contain the IP address of the proxy or load balancer only.
+// To see the original IP address of the client, the X-Forwarded-For
+// request header is used.
+// See More : https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+app.set('trust proxy', true);
+
+// Here we have disabled the cookie encryption with signed: false flag, because
+// JWT that we are doing to use already secure and no need to encrypt.
+//
+// But if we planing to store more sensitive data on cookie we need to encrypt in
+// such case.
+app.use(cookieSession({
+    // Disable encryption on cookies.
+    signed: false,
+
+    // Indicates whether the cookie is only to be sent over HTTPS
+    // Also if true in node test env, we won't be able to test cookie in the test cases
+    // Hence we disable setting cookie only on HTTPS, when in test env.
+    secure: process.env.NODE_ENV !== 'test',
+}));
+
+
+// Send 404 for all not found routes, 'all' for all type of HTTP methods, GET, POST, DELETE etc.
+app.all('*', async () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
+
+
