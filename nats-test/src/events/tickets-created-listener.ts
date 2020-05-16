f58d713271ce45220cd55a629fa72dd3fb6c0181
chamikabm <chamika@bitsmedia.com>
2020-05-16 16:14:15 +0530

    Refactored listener files.

--- nats-test/src/events/tickets-created-listener.ts
+++ nats-test/src/events/tickets-created-listener.ts
@@ -1,12 +1,16 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketsCreatedEvent } from './tickets-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketsCreatedEvent> {
+    // subject: TicketsCreatedEvent['subject'] =  Subjects.TicketCreated;  Also works
+    subject: Subjects.TicketCreated =  Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketsCreatedEvent['data'], msg: Message): void {
         console.log('Event data!', data);
+        console.log(data.id);
         msg.ack();
     }
 }
