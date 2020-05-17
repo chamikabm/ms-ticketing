fedf7eaed574602418cfe01e1f58c0d85b21f6a5
chamikabm <chamika@bitsmedia.com>
2020-05-17 10:54:03 +0530

    OrderTypes enum added.

---
+++ orders/src/models/order.ts
@@ -0,0 +1,83 @@
+import mongoose from 'mongoose';
+import { Ticket } from '../../../tickets/src/models/ticket';
+import { OrderStatus } from '@ms-ticketing/common';
+
+// An interface that describes the properties
+// that are required to creat a new order.
+interface OrderAttrs {
+    userId: string,
+    status: OrderStatus,
+    expiresAt: Date,
+    ticket: TicketDoc,
+}
+
+// An interface that describes the properties
+// that a Order Document has.
+interface OrderDoc extends mongoose.Document {
+    userId: string,
+    status: OrderStatus,
+    expiresAt: Date,
+    ticket: TicketDoc,
+}
+
+// An interface that describes the properties
+// that a Order Model has.
+interface OrderModel extends mongoose.Model<OrderDoc> {
+    build(attrs: OrderAttrs): OrderDoc,
+}
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(OrderStatus),
+    },
+    expiresAt: {
+        type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ticket',
+    },
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+        }
+    }
+});
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order(attrs);
+};
+
+// const Order = mongoose.model('Order', orderSchema);
+// New need to add '<any, OrderModel>' to bind typescript model to mongoose model.
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+// We create this builder function to get help with typescript when creating
+// orders, otherwise mongoose Order model does not have type validations. Having such
+// builder allow us to validate the Order attributes through the OrderAttrs interface
+// when passing to the `buildOrder` function.
+// const buildOrder = (attrs: OrderAttrs) => {
+//     return new Order(attrs);
+// };
+
+// export { Order, buildOrder };
+
+// Without doing above, we can export Single `Order` model as we normally do
+// by using 'orderSchema.statics.build' to attach a function to create orders
+// as above, so that we can simply create orders as below.
+
+// Order.build({
+//     userId: '',
+//     status: ''
+// })
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,22 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@ms-ticketing/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders',
+    requireAuth,
+    [
+        body('title')
+            .not().isEmpty()
+            .custom(((input:string) => mongoose.Types.ObjectId.isValid(input)))
+            .withMessage('Valid ticket id must be provided.'),
+        body('price').isFloat({ gt: 0 })
+            .withMessage('Price must be greater than 0'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
 
     res.send({ });
 });
