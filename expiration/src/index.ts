449dc66baf5a0c3cc07afd46e06467eaa4aeca61
chamikabm <chamika@bitsmedia.com>
2020-05-25 21:08:13 +0530

    Expiration services initial files and k8 files added.

---
+++ expiration/src/index.ts
@@ -0,0 +1,45 @@
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+    if(!process.env.NATS_URL) {
+        throw Error('NATS_URL must be defined.');
+    }
+
+    if(!process.env.NATS_CLUSTER_ID) {
+        throw Error('NATS_CLUSTER_ID must be defined.');
+    }
+
+    if(!process.env.NATS_CLIENT_ID) {
+        throw Error('NATS_CLIENT_ID must be defined.');
+    }
+
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL,
+        );
+
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!');
+            process.exit();
+        });
+
+        process.on('SIGINT', () => {
+            // Exit by CMD + C etc.
+            console.log('SIGINT Received!')
+            natsWrapper.client.close();
+        });
+
+        process.on('SIGTERM', () => {
+            // Restart
+            console.log('SIGTERM Received!')
+            natsWrapper.client.close();
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
